9bcf3ba9e27944266ff5d05a0a2cdbb09534376f
emkeator <emkeator@gmail.com>
2017-09-20 11:40:52 -0600

    done

---
+++ js/service.js
@@ -0,0 +1,9 @@
+angular.module('userProfiles').service('mainService', function($http) {
+
+  this.getUsers = function(){
+      return $http({
+          method: 'GET',
+          url: `https://reqres.in/api/users?page=1`
+        });
+  }
+})
